106453e75d0d4c353e87a91f4141992e59ddb314
simonediberardino <simonediberardino7@gmail.com>
2022-07-07 18:34:21 +0200

    TODO: Remove car class

--- app/src/main/java/com/mini/infotainment/entities/Car.kt
+++ app/src/main/java/com/mini/infotainment/entities/Car.kt
@@ -1,11 +1,9 @@
 package com.mini.infotainment.entities
 
 import android.location.Location
-import com.mini.infotainment.support.LocationExtended
 
-class Car(var targa: String, var carName: String, var password: String, var location: Location?) {
+class Car(var location: Location?) {
     companion object{
-        val currentCar = Car("CW832LX", "Mini 1.4 TDI One D", "3238", null)
+        val currentCar = Car(null)
     }
-
 }
