da4afb302c53d1bcc41ce03079eaef227d37e3d3
simonediberardino <simonediberardino7@gmail.com>
2022-07-20 00:42:51 +0200

    Icons animations

--- app/src/main/java/com/mini/infotainment/activities/home/AppsMenu.kt
+++ app/src/main/java/com/mini/infotainment/activities/home/AppsMenu.kt
@@ -3,13 +3,16 @@ package com.mini.infotainment.activities.home
 import android.content.Intent
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateInterpolator
 import android.view.animation.Animation
+import android.view.animation.ScaleAnimation
 import android.view.animation.TranslateAnimation
 import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.mini.infotainment.R
 import com.mini.infotainment.support.AppInfo
 import com.mini.infotainment.support.Page
+
 
 class AppsMenu(override val ctx: HomeActivity) : Page() {
     companion object {
@@ -18,7 +21,7 @@ class AppsMenu(override val ctx: HomeActivity) : Page() {
     }
     
     internal lateinit var containAppDrawer: ConstraintLayout
-    internal var isAppDrawerVisible = false
+    internal var isAppDrawerVisible = true
     internal lateinit var grdView: GridView
 
     override fun build() {
@@ -26,12 +29,11 @@ class AppsMenu(override val ctx: HomeActivity) : Page() {
         adapter = null
 
         containAppDrawer = ctx.findViewById(R.id.home_containAppDrawer)
-        containAppDrawer.visibility = View.INVISIBLE
-        
+
         loadApps()
         loadListView()
         addGridListeners()
-        
+
         this.show(false, 0)
     }
 
@@ -85,7 +87,7 @@ class AppsMenu(override val ctx: HomeActivity) : Page() {
                 }
             }
         }
-        
+
         grdView.adapter = adapter
     }
 
@@ -103,68 +105,135 @@ class AppsMenu(override val ctx: HomeActivity) : Page() {
         if(visibility == isAppDrawerVisible)
             return
 
-        grdView.visibility = if(visibility) View.VISIBLE else View.INVISIBLE
         isAppDrawerVisible = visibility
 
         if (visibility) {
-            slideMenuUp(duration)
+            slideAppsMenuUp(duration)
+            hideMainMenu(duration)
         } else {
-            slideMenuDown(duration)
+            showMainMenu(duration)
+            slideAppsMenuDown(duration)
         }
     }
 
-    private fun slideMenuUp(duration: Long) {
-        val background = ctx.findViewById<View>(R.id.home_background)
-        background.alpha = 0f
-        background.animate().alpha(0.4f).duration = duration
+    private fun showMainMenu(duration: Long){
+        val mainMenu = ctx.findViewById<View>(R.id.home_container)
 
-        containAppDrawer.visibility = View.VISIBLE
+        alphaAnimation(
+            mainMenu,
+            0f,
+            1f,
+            duration
+        )
 
+        moveAnimation(
+            mainMenu,
+            0f,
+            0f,
+            -mainMenu.height.toFloat(),
+            0f,
+            duration,
+            null
+        )
+
+        scaleViewAnimation(
+            mainMenu,
+            3f,
+            1f,
+            duration
+        )
+    }
+
+    private fun hideMainMenu(duration: Long){
+        val mainMenu = ctx.findViewById<View>(R.id.home_container)
+
+        alphaAnimation(mainMenu, 1f, 0f, duration)
+        moveAnimation(mainMenu, 0f, 0f, 0f, -mainMenu.height.toFloat(), duration, null)
+        scaleViewAnimation(mainMenu, 1f, 3f, duration)
+    }
+
+    private fun alphaAnimation(v: View, startAlpha: Float, endAlpha: Float, duration: Long){
+        v.alpha = startAlpha
+        v.animate().alpha(endAlpha).duration = duration
+    }
+    
+    private fun moveAnimation(v: View, startX: Float, endX: Float, startY: Float, endY: Float, duration: Long, callback: Runnable?){
         val slideAnimation = TranslateAnimation(
+            startX,
+            endX,
+            startY,
+            endY
+        )
+
+        slideAnimation.interpolator = AccelerateInterpolator(2f)
+        slideAnimation.duration = duration
+        slideAnimation.fillAfter = true
+        slideAnimation.setAnimationListener(object: Animation.AnimationListener{
+            override fun onAnimationStart(p0: Animation?) {}
+            override fun onAnimationRepeat(p0: Animation?) {}
+
+            override fun onAnimationEnd(p0: Animation?) {
+                callback?.run()
+            }
+        })
+        
+        v.startAnimation(slideAnimation)
+    }
+    
+    private fun scaleViewAnimation(v: View, startScale: Float, endScale: Float, duration: Long) {
+        val anim: Animation = ScaleAnimation(
+            startScale,
+            endScale,
+            startScale, endScale,
+            Animation.RELATIVE_TO_SELF, 0f,
+            Animation.RELATIVE_TO_SELF, 1f
+        )
+
+        anim.fillAfter = true
+        anim.duration = duration
+        anim.interpolator = AccelerateInterpolator(2f)
+
+        v.startAnimation(anim)
+    }
+
+    private fun slideAppsMenuUp(duration: Long) {
+        grdView.visibility = View.VISIBLE
+
+        alphaAnimation(
+            containAppDrawer,
+            0f,
+            1f,
+            duration
+        )
+        
+        moveAnimation(
+            containAppDrawer,
             0f,
             0f,
             containAppDrawer.height.toFloat(),
-            0f
+            0f,
+            duration,
+            null
         )
-
-        slideAnimation.setAnimationListener(object : Animation.AnimationListener{
-            override fun onAnimationRepeat(p0: Animation?) {}
-            override fun onAnimationStart(p0: Animation?) {}
-
-            override fun onAnimationEnd(p0: Animation?) {
-                containAppDrawer.visibility = View.VISIBLE
-            }
-        })
-
-        slideAnimation.duration = duration
-        slideAnimation.fillAfter = false
-        containAppDrawer.startAnimation(slideAnimation)
     }
 
-    private fun slideMenuDown(duration: Long) {
-        val background = ctx.findViewById<View>(R.id.home_background)
-        background.alpha = 0.4f
-        background.animate().alpha(0f).duration = duration
-
-        val slideAnimation = TranslateAnimation(
+    private fun slideAppsMenuDown(duration: Long) {
+        alphaAnimation(
+            containAppDrawer,
+            1f, 
             0f,
-            0f,
-            0f,
-            containAppDrawer.height.toFloat()
+            duration
         )
-
-        slideAnimation.duration = duration
-        slideAnimation.fillAfter = false
-        slideAnimation.setAnimationListener(object : Animation.AnimationListener{
-            override fun onAnimationEnd(p0: Animation?) {
-                containAppDrawer.visibility = View.INVISIBLE
-            }
-
-            override fun onAnimationStart(p0: Animation?) {}
-            override fun onAnimationRepeat(p0: Animation?){}
-        })
-
-        containAppDrawer.startAnimation(slideAnimation)
+        
+        moveAnimation(
+            containAppDrawer,
+            0f, 
+            0f, 
+            0f, 
+            containAppDrawer.height.toFloat(), 
+            duration
+        ){
+            grdView.visibility = View.INVISIBLE
+        }
     }
-
 }
